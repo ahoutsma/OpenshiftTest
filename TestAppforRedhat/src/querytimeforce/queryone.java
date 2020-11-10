1967855c48c779b39c33b9eb3b771f6848d3fda9
aaron.houtsma <aaron.houtsma@host.docker.internal>
2020-11-10 09:28:27 -0700

    A test for openshift ah

---
+++ TestAppforRedhat/src/querytimeforce/queryone.java
@@ -0,0 +1,22 @@
+package querytimeforce;
+import java.sql.*;
+/**
+ * A JDBC SELECT (JDBC query) example program.
+ */
+class Query1 {
+ 
+    public static void main (String[] args) {
+    	
+    	
+    	System.out.println("A test message for redhat deployment");
+   
+
+
+ 
+
+   
+
+
+    }
+
+}
